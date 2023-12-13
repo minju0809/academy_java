806e0012a9fc9476be6f9b2ee6b0b55e4e327113
minju <loverminju88@naver.com>
2023-12-13 13:06:49 +0900

    231213 수업 진행 - db연동, server에서 값 입력까지

---
+++ sample.js
@@ -0,0 +1,106 @@
+let http = require('http');
+let express = require('express');
+let app = express();
+
+let items = [
+  { name: '우유', price: '2000', etc: '몸에 좋은 우유' },
+  { name: '홍차', price: '3000', etc: '몸에 좋은 홍차' },
+  { name: '커피', price: '4000', etc: '몸에 좋은 커피' },
+  { name: '쌍화차', price: '4500', etc: '몸에 좋은 쌍화차' }
+];
+
+app.use(express.bodyParser());
+app.use(express.static('public')); // 폴더 접근
+app.use(app.router);
+
+app.all('/data.xml', (request, response) => {
+  let output = '';
+  output += '<?xml version="1.0" encoding="UTF-8" ?>';
+  output += '<products>'
+  items.forEach((item) => {
+    output += '<product>'
+    output += '<name>' + item.name + '</name>';
+    output += '<price>' + item.price + '</price>';
+    output += '</product>'
+  });
+  output += '</products>'
+  response.type('text/xml');
+  response.send(output);
+});
+
+app.all('/data.json', (request, response) => {
+  response.send(items);
+});
+
+app.all('/data.html', (request, response) => {
+  let output = "";
+  items.forEach((item) => {
+    output += `${item.name} :: ${item.price} <br>`;
+  });
+  response.send(output);
+});
+
+app.get('/detail/:idx', (request, response) => {
+  let idx = Number(request.params.idx);
+  idx = idx - 1;
+  if (isNaN(idx)) {
+    response.send({ error: '숫자를 입력하세요!!' });
+  } else if (items[idx]) {
+    response.send(items[idx].etc);
+  } else {
+    response.send({ error: '존재하지 않는 데이터 입니다.!!' });
+  }
+});
+
+app.post('/insert', (request, response) => {
+  let name = request.body.name;
+  let price = request.body.price;
+  let item = { name: name, price: price };
+  items.push(item);
+
+  response.send({
+    message: '데이터를 추가했습니다.', data: item
+  });
+});
+
+app.get('/select', (request, response) => {
+  response.send(items);
+});
+
+app.del('/delete/:idx', function (request, response) {
+  let idx = Number(request.params.idx);
+  idx = idx - 1;
+  if (isNaN(idx)) {
+    response.send({ error: '숫자를 입력하세요!!' });
+  } else if (items[idx]) {
+    items.splice(idx, 1);
+    response.send({ message: '데이터를 삭제 했습니다.!' });
+  } else {
+    response.send({ error: '존재하지 않는 데이터입니다.!' });
+  }
+});
+
+app.put('/edit', (request, response) => {
+  let idx = request.body.idx;
+  idx = idx - 1;
+  let name = request.body.name;
+  let price = request.body.price;
+
+  if (isNaN(idx)) {
+    response.send({ error: '숫자를 입력하세요!!' });
+  } else if (items[idx]) {
+    if (name) { items[idx].name = name; }
+    if (price) { items[idx].price = price; }
+    response.send({ message: '데이터를 수정했습니다.', data: items[idx] });
+  } else {
+    response.send({ error: '존재하지 않는 데이터 입니다.' });
+  }
+});
+
+app.use((request, response, next) => {
+  response.send('<h1> Hello Middleware ...! </h1>');
+});
+
+http.createServer(app).listen(52273, () => {
+  console.log('Server Running at http://127.0.0.1:52273');
+});
--- server.js
+++ server.js
@@ -1,106 +1,148 @@
 let http = require('http');
 let express = require('express');
 let app = express();
+let oracledb = require("oracledb");
 
-let items = [
-  { name: '우유', price: '2000', etc: '몸에 좋은 우유' },
-  { name: '홍차', price: '3000', etc: '몸에 좋은 홍차' },
-  { name: '커피', price: '4000', etc: '몸에 좋은 커피' },
-  { name: '쌍화차', price: '4500', etc: '몸에 좋은 쌍화차' }
-];
-
+app.use(express.logger());
 app.use(express.bodyParser());
-app.use(express.static('public')); // 폴더 접근
+app.use(express.static('public'));
 app.use(app.router);
 
-app.all('/data.xml', (request, response) => {
-  let output = '';
-  output += '<?xml version="1.0" encoding="UTF-8" ?>';
-  output += '<products>'
-  items.forEach((item) => {
-    output += '<product>'
-    output += '<name>' + item.name + '</name>';
-    output += '<price>' + item.price + '</price>';
-    output += '</product>'
+//  오라클에서는 autoCommit 속성을 부여해야 적용된다.!!
+oracledb.autoCommit = true;
+oracledb.initOracleClient(); // 버전 문제 해결
+
+let conn = null;
+oracledb.getConnection({
+  user: "system",
+  password: "1234",
+  connectString: "localhost/xe"
+}, (err, con) => {
+  if (err) {
+    // console.log("접속 실패", err);
+  } else {
+    conn = con;
+    // console.log("접속 성공", conn);
+  }
+});
+
+app.get('/select/', function (request, response) {
+  let SQL = "select * from memo1213";
+  conn.execute(SQL, function (err, data) {
+
+    if (err) {
+      console.log("==> 등록중 에러가 발생했어요!!", err);
+      response.writeHead(500, { "ContentType": "text/html" });
+      response.end("fail!!");
+    } else {
+      console.log("===> result : ", data);
+      response.send(data.rows);
+    }
   });
-  output += '</products>'
-  response.type('text/xml');
-  response.send(output);
 });
 
-app.all('/data.json', (request, response) => {
-  response.send(items);
-});
+app.get('/detail/', function (request, response) {
+  let idx = '101';
+  let SQL = "select * from memo1213 where idx = :idx";
+  conn.execute(SQL,[idx], function (err, data) {
 
-app.all('/data.html', (request, response) => {
-  let output = "";
-  items.forEach((item) => {
-    output += `${item.name} :: ${item.price} <br>`;
+    if (err) {
+      console.log("==> 등록중 에러가 발생했어요!!", err);
+      response.writeHead(500, { "ContentType": "text/html" });
+      response.end("fail!!");
+    } else {
+      console.log("===> result : ", data);
+      response.send(data.rows);
+    }
   });
-  response.send(output);
 });
 
-app.get('/detail/:idx', (request, response) => {
+app.get('/detail2/', function (request, response) {
+  let idx = '102';
+  let SQL = "select * from memo1213 where idx = " + idx;
+  conn.execute(SQL, function (err, data) {
+    
+    if (err) {
+      console.log("==> 등록중 에러가 발생했어요!!", err);
+      response.writeHead(500, { "ContentType": "text/html" });
+      response.end("fail!!");
+    } else {
+      console.log("===> result : ", data);
+      response.send(data.rows);
+    }
+  });
+});
+
+app.all('/insert/', function (request, response) {
+  let writer = '바다';
+  let memo = '일본 여행';
+  let SQL = "insert into memo1213 (idx, writer, memo) values (idx.nextval, :writer, :memo)";
+  conn.execute(SQL,[writer, memo], function (err, data) {
+    
+    if (err) {
+      console.log("==> 등록중 에러가 발생했어요!!", err);
+      response.writeHead(500, { "ContentType": "text/html" });
+      response.end("fail!!");
+    } else {
+      console.log("===> result(저장성공) : ", data);
+    }
+  });
+});
+
+app.all('/insert2/', function (request, response) {
+  let writer = '월출이';
+  let memo = '울릉도 여행';
+  let SQL = "insert into memo1213 (idx, writer, memo) " 
+    SQL += " values (idx.nextval, '" + writer + "', '" + memo + "')";
+  conn.execute(SQL, function (err, data) {
+    
+    if (err) {
+      console.log("==> 등록중 에러가 발생했어요!!", err);
+      response.writeHead(500, { "ContentType": "text/html" });
+      response.end("fail!!");
+    } else {
+      console.log("===> result(저장성공) : ", data);
+    }
+  });
+});
+
+app.all('/delete/:idx', function (request, response) {
   let idx = Number(request.params.idx);
-  idx = idx - 1;
-  if (isNaN(idx)) {
-    response.send({ error: '숫자를 입력하세요!!' });
-  } else if (items[idx]) {
-    response.send(items[idx].etc);
-  } else {
-    response.send({ error: '존재하지 않는 데이터 입니다.!!' });
-  }
-});
-
-app.post('/insert', (request, response) => {
-  let name = request.body.name;
-  let price = request.body.price;
-  let item = { name: name, price: price };
-  items.push(item);
-
-  response.send({
-    message: '데이터를 추가했습니다.', data: item
+  let SQL = "delete from memo1213 where idx = :idx";
+  conn.execute(SQL,[idx], function (err, data) {
+    
+    if (err) {
+      console.log("==> 등록중 에러가 발생했어요!!", err);
+      response.writeHead(500, { "ContentType": "text/html" });
+      response.end("fail!!");
+    } else {
+      console.log("===> result(삭제성공) : ", data);
+    }
   });
 });
 
-app.get('/select', (request, response) => {
-  response.send(items);
+http.createServer(app).listen(52273, function () {
+  console.log('Server Running  at  http://127.0.0.1:52273');
 });
 
-app.del('/delete/:idx', function (request, response) {
-  let idx = Number(request.params.idx);
-  idx = idx - 1;
-  if (isNaN(idx)) {
-    response.send({ error: '숫자를 입력하세요!!' });
-  } else if (items[idx]) {
-    items.splice(idx, 1);
-    response.send({ message: '데이터를 삭제 했습니다.!' });
+
+
+
+
+//////////////////////////////////////////////////////
+
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+  user: 'root',
+  password: 'autoset',
+  database: 'node'
+});
+
+client.query('select * from  memo1213', function (error, result, fields) {
+  if (error) {
+    // console.log('쿼리 문장에 오류가 있습니다.');
   } else {
-    response.send({ error: '존재하지 않는 데이터입니다.!' });
+    // console.log(result);
   }
-});
-
-app.put('/edit', (request, response) => {
-  let idx = request.body.idx;
-  idx = idx - 1;
-  let name = request.body.name;
-  let price = request.body.price;
-
-  if (isNaN(idx)) {
-    response.send({ error: '숫자를 입력하세요!!' });
-  } else if (items[idx]) {
-    if (name) { items[idx].name = name; }
-    if (price) { items[idx].price = price; }
-    response.send({ message: '데이터를 수정했습니다.', data: items[idx] });
-  } else {
-    response.send({ error: '존재하지 않는 데이터 입니다.' });
-  }
-});
-
-app.use((request, response, next) => {
-  response.send('<h1> Hello Middleware ...! </h1>');
-});
-
-http.createServer(app).listen(52273, () => {
-  console.log('Server Running at http://127.0.0.1:52273');
 });
